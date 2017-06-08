7b828eb9e0c2901d5dd4be704a8b05fcb067fdf7
Kar <karimcambridge@gmail.com>
2017-06-08 14:39:52 -0400

    Update cargo inventory to support clicking on specific airwaybills

--- cargoinventory.php
+++ cargoinventory.php
@@ -3,10 +3,19 @@
 <?php
   include("includes/paginator.php");
 
+  $useAirWayBill;
+  if(isset( $_GET['airwaybill'] )) {
+    $useAirWayBill = $_GET['airwaybill'];
+  }
+
   $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
   $page       = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
   $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 15;
-  $query      = "SELECT `airwaybill`, `cargo_item_types`.`cargo_type` AS `cargo_type`, `item_description`, `item_weight`, `date_in`, `state`, `count` FROM `cargo_inventory`, `cargo_item_types` WHERE `cargo_inventory`.`cargo_type_id` = `cargo_item_types`.`ID` ORDER BY `date_in`,`airwaybill` DESC";
+  $query      = 'SELECT `airwaybill`, `cargo_item_types`.`cargo_type` AS `cargo_type`, `item_description`, `item_weight`, `date_in`, `state`, `count` FROM `cargo_inventory`, `cargo_item_types` WHERE `cargo_inventory`.`cargo_type_id` = `cargo_item_types`.`ID` ';
+  if(!empty($useAirWayBill)) {
+    $query .= "AND `airwaybill` = '" . $useAirWayBill . "'";
+  }
+  $query .= 'ORDER BY `date_in`,`airwaybill` DESC';
  
   $Paginator  = new Paginator( $connectionHandle, $query );
 
@@ -14,12 +23,13 @@
 ?>
 <?php
     if(isset($results) == true) {
-      $sql = "SELECT `airwaybill`, COUNT(`airwaybill`) AS `maxcount` FROM `cargo_inventory` GROUP BY `airwaybill` LIMIT " . ( ( $results->page - 1 ) * $results->limit ) . ", $results->limit";
-      $result = $connectionHandle->query($sql);
-      while($row = $result->fetch_assoc()) {
-        for( $i = 0; $i < count( $results->data ); $i++ ) {
-          if($results->data[$i]['airwaybill'] == $row['airwaybill']) {
-            $results->data[$i]['maxcount'] = $row['maxcount'];
+      $sql = 'SELECT `airwaybill`, COUNT(`airwaybill`) AS `maxcount` FROM `cargo_inventory` GROUP BY `airwaybill` LIMIT ' . ( ( $results->page - 1 ) * $results->limit ) . ',' . $results->limit;
+      if($result = $connectionHandle->query($sql)) {
+        while($row = $result->fetch_assoc()) {
+          for( $i = 0; $i < count( $results->data ); $i++ ) {
+            if($results->data[$i]['airwaybill'] == $row['airwaybill']) {
+              $results->data[$i]['maxcount'] = $row['maxcount'];
+            }
           }
         }
       }
@@ -29,7 +39,14 @@
   <div class="col-md-12">
 	<ol class="breadcrumb">
 	  <li><a href="dashboard.php">Home</a></li>
-	  <li class="active"><strong>Cargo Inventory</strong></li>
+    <?php
+      if(!empty($useAirWayBill)) {
+        echo "<li><a href=\"" . $_SERVER['SCRIPT_NAME'] . "\">Cargo Inventory</a></li>";
+        echo "<li class=\"active\">" . $useAirWayBill . "</li>";
+      } else {
+        echo "<li class=\"active\"><strong>Cargo Inventory</strong></li>";
+      }
+    ?>
 	</ol>
     <div class="panel panel-primary">
       <div class="panel-heading">Cargo Inventory</div>
@@ -53,7 +70,7 @@
       	  		 	 for( $i = 0; $i < count( $results->data ); $i++ ) {
       	  		 	 	echo "<tr>";
 
-      	  		 	 	echo "<td>" . $results->data[$i]['airwaybill'] . "</td>";
+      	  		 	 	echo "<td><a href=" . $_SERVER['SCRIPT_NAME'] . "?airwaybill=" . $results->data[$i]['airwaybill'] . ">" . $results->data[$i]['airwaybill'] . "</a></td>";
                   echo "<td>" . $results->data[$i]['cargo_type'] . "</td>";
                   echo "<td>" . $results->data[$i]['item_description'] . "</td>";
                   echo "<td>" . $results->data[$i]['item_weight'] . "</td>";
