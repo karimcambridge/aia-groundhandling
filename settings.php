9da2debad8fec8f946cd449e6ea600d309f7c239
Kar <karimcambridge@gmail.com>
2017-06-15 15:38:48 -0400

    Update settings

--- settings.php
+++ settings.php
@@ -8,8 +8,16 @@
 			</ol>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header"><strong><?php echo 'Hello ' . $_SESSION['accountUsername'] . '. You are a ' . USER_LEVEL_NAME[$_SESSION['accountLevel']] . ' User.'; ?></strong></div>
+				<div class="card-block">
+					
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-
-<?php echo 'Hello ' $_SESSION['accountUsername'] . '. You are of level ' . USER_LEVEL_NAME[$_SESSION['accountLevel']]; ?>
 
 <?php require_once 'includes/footer.php'; ?>
