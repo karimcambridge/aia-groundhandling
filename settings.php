661b11336d1f78f6a43cc4462ecd1a9eb43a3972
Kar <karimcambridge@gmail.com>
2017-06-19 09:22:22 -0400

    Settings layout slight change

--- settings.php
+++ settings.php
@@ -14,36 +14,40 @@
 				<div class="card-header"><strong><?php echo 'Hello ' . $_SESSION['accountUsername'] . '. You are a ' . USER_LEVEL_NAME[$_SESSION['accountLevel']] . ' User.'; ?></strong></div>
 				<div class="card-block">
 					<div class="row">
-						<div class="col-lg-3">
-							<button type="button" class="btn btn-outline-info" id="user-list-button">List Users</button>
-							<button type="button" class="btn btn-outline-primary" id="user-add-button">Add User</button>
-							<button type="button" class="btn btn-outline-warning" id="user-disable-button">Disable User</button>
-							<button type="button" class="btn btn-outline-danger btn-sm" id="user-delete-button">Delete User</button>
+						<div class="col-lg-1">
+							<h3>Users</h3>
+							<button type="button" class="btn btn-outline-info" id="user-list-button">List</button>
+							<button type="button" class="btn btn-outline-primary" id="user-add-button">Add</button>
+							<button type="button" class="btn btn-outline-warning" id="user-disable-button">Disable</button>
+							<button type="button" class="btn btn-outline-danger btn-sm" id="user-delete-button">Delete</button>
 						</div>
-						<div class="col-lg-3">
-							<button type="button" class="btn btn-outline-info" id="itemtype-list-button">List Item Types</button>
-							<button type="button" class="btn btn-outline-warning" id="itemtype-add-button">Add Item Type</button>
-							<button type="button" class="btn btn-outline-danger btn-sm" id="itemtype-delete-button">Delete Item Type</button>
+						<div class="col-lg-2">
+							<h3>Item Types</h3>
+							<button type="button" class="btn btn-outline-info" id="itemtype-list-button">List</button>
+							<button type="button" class="btn btn-outline-warning" id="itemtype-add-button">Add</button>
+							<button type="button" class="btn btn-outline-danger btn-sm" id="itemtype-delete-button">Delete</button>
 						</div>
-						<div class="col-lg-3">
-							<button type="button" class="btn btn-outline-info" id="carrier-list-button">List Carrier</button>
-							<button type="button" class="btn btn-outline-warning" id="carrier-add-button">Add Carrier</button>
-							<button type="button" class="btn btn-outline-danger btn-sm" id="carrier-delete-button">Delete Carrier</button>
+						<div class="col-lg-1">
+							<h3>Carrier</h3>
+							<button type="button" class="btn btn-outline-info" id="carrier-list-button">List</button>
+							<button type="button" class="btn btn-outline-warning" id="carrier-add-button">Add</button>
+							<button type="button" class="btn btn-outline-danger btn-sm" id="carrier-delete-button">Delete</button>
 						</div>
-						<div class="col-lg-3">
-							<button type="button" class="btn btn-outline-info" id="consignee-list-button">List Consignee</button>
-							<button type="button" class="btn btn-outline-warning" id="consignee-add-button">Add Consignee</button>
-							<button type="button" class="btn btn-outline-danger btn-sm" id="consignee-delete-button">Delete Consignee</button>
+						<div class="col-lg-1">
+							<h3>Consignee</h3>
+							<button type="button" class="btn btn-outline-info" id="consignee-list-button">List</button>
+							<button type="button" class="btn btn-outline-warning" id="consignee-add-button">Add</button>
+							<button type="button" class="btn btn-outline-danger btn-sm" id="consignee-delete-button">Delete</button>
 						</div>
 					</div>
 					<div class="row">
-						<div class="col-lg-3" id="user-group">
+						<div class="col-lg-1" id="user-group">
 						</div>
-						<div class="col-lg-3" id="item-type-group">
+						<div class="col-lg-2" id="item-type-group">
 						</div>
-						<div class="col-lg-3" id="carrier-group">
+						<div class="col-lg-1" id="carrier-group">
 						</div>
-						<div class="col-lg-3" id="consignee-group">
+						<div class="col-lg-1" id="consignee-group">
 						</div>
 					</div>
 				</div>
